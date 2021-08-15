95b976b05dfe3daa2211cbfbc5c1df5e34bafa4b
Kmerck88 <kevin.merck88@gmail.com>
2021-08-15 16:33:22 -0400

    Setting up Budget app

---
+++ Budget.py
@@ -0,0 +1,24 @@
+class Category:
+    def _init(self, name):
+        self.category = category
+
+    def deposit(self):
+           pass
+
+    def withdraw(self):
+           pass
+
+def get_balance(self):
+          pass
+def transfers(self):
+          pass
+def check_funds(self):
+          pass
+
+
+
+cat = Category("Food")
+print(cat.name)
+
+def create_spend_chart(catrgories):
+    pass
